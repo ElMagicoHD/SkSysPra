69fc7d88927427f62f57c2ae0e8f6aecbb43d637
Alexander Linger <linger@campus.tu-berlin.de>
2020-05-12 15:50:09 +0200

    added js for strikethrough + char remaining count

---
+++ HA01/scripting.js
@@ -0,0 +1,21 @@
+var e;
+e = document.getElementById("textfeld");
+e.addEventListener("keyup", charcounter, false);
+
+function charcounter(){
+    t = document.getElementById("textfeld");
+    start = 160;
+    remainingchar = 160 - t.value.length;
+
+    document.getElementById("remaining").innerHTML = remainingchar + " characters left";
+}
+
+function strikethrough(x){
+    let a = 't' + x;
+    document.getElementById(a).style.textDecoration = "line-through";
+}
+
+function restoring(x){
+    let a = 't' + x;
+    document.getElementById(a).style.textDecoration = "none";
+}
