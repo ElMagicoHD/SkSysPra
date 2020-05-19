0696eece952e3fd1fcd4323df9defef66982bfbd
ElMagicoHD <48961123+ElMagicoHD@users.noreply.github.com>
2020-05-19 09:53:58 +0200

    adding todos works now

--- HA02/Website/todo/urls.py
+++ HA02/Website/todo/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.overview, name='overview'),
-    path('new', views.newTodo, name='new-todo'),
-    path('edit', views.edit, name='edit-todo')
+    path('new/', views.newTodo, name='new-todo'),
+    path('edit/', views.edit, name='edit-todo'),
+    path('addTodo/', views.addTodo, name='add-todo')
 ]
--- HA02/Website/todo/views.py
+++ HA02/Website/todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Todo
 
 
@@ -24,3 +25,18 @@ def edit(request):
         'title': 'Edit',
     } 
     return render(request, 'todo/edit.html', context)
+
+def addTodo(request):
+    #getting info from httpPost
+    c = request.POST['task']
+    d = request.POST['due_date']
+    p = request.POST['progressScrollerInput']
+    #creating Todo Object from received Inputs
+    temp = Todo(task=c, due_date=d, progress = p)
+    #saving item
+    temp.save()
+    #redirecting
+    return HttpResponseRedirect('/')
+
+
+
